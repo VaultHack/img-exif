683093662147ddbd1945a91fbd5e12bec1740237
Frank Cleary <fcleary@index.com>
2015-07-05 16:22:13 -0700

    hash changes

--- exif_data.py
+++ exif_data.py
@@ -1,5 +1,4 @@
 import os
-import hashlib
 import pandas as pd
 
 from PIL import Image
@@ -10,38 +9,35 @@ def process_base_dirs(base_dirs):
     # md5 hashes of the files we've extracted data for
     files_scanned = set()
     exif_data = []
-    for base_dir in [base_dirs]:
+    if isinstance(base_dirs, str):
+        base_dirs = [base_dirs]
+    for base_dir in base_dirs:
+        print base_dirs
         for root, dirs, files in os.walk(base_dir):
             for file_name in files:
                 if file_name.lower().endswith('.jpg'):
                     file_path = os.path.join(root, file_name)
-                    md5 = get_md5(file_path)
-                    if md5 in files_scanned:
+                    img_hash, img_exif = get_exif_data(file_path)
+                    if img_hash in files_scanned:
                         continue
                     else:
-                        files_scanned.add(md5)
-                        exif_data.append(get_exif_data(file_path))
+                        files_scanned.add(img_hash)
+                        exif_data.append(img_exif)
     return exif_data
-
-def get_md5(file_path, block_size=2**20):
-    m = hashlib.md5()
-    with open(file_path, 'rb') as f:
-        while True:
-            buf = f.read(block_size)
-            if not buf:
-                break
-            m.update(buf)
-    return m.digest()
 
 
 def get_exif_data(file_path):
     img = Image.open(file_path)
-    raw_exif = img._getexif()
+    img_hash = hash(tuple(img.getdata()))
+    try:
+        raw_exif = img._getexif()
+    except AttributeError:
+        print file_path
     if raw_exif is None:
-        return {}
+        return img_hash, {}
     else:
-        return {ExifTags.TAGS[k]: v for k, v in raw_exif.items()
-                if k in ExifTags.TAGS}
+        return img_hash, {ExifTags.TAGS[k]: v for k, v in raw_exif.items()
+                          if k in ExifTags.TAGS}
 
 
 def make_df(exif_data, rows=('FocalLength')):
@@ -57,11 +53,10 @@ def get_df(base_dirs, rows=('FocalLength')):
 
 if __name__ == '__main__':
     base = '/Users/frank/Pictures'
-    base_dirs = ['Photos Library.photoslibrary/Masters/2015/03/21']
+    base_dirs = ['Photos Library.photoslibrary/Masters/2015/05/31']
     base_dirs = [os.path.join(base, dir) for dir in base_dirs]
 
     rows=['DateTimeOriginal', 'FocalLength', 'Make', 'Model']
     result = make_df(process_base_dirs(base_dirs), rows=rows)
 
     print result.head()
-
